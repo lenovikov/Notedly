e4cca025dc0f6595f847ced7e370d9cc1a0601a1
lenovikov <le.novikovv@gmail.com>
2023-01-24 18:14:17 +0300

    [Note-1]-create server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-// index.js
-// This is the main entry point of our application
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4001;
+
+// Строим схему с помощью языка схем GraphQL
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Предоставляем функции распознавания для полей схемы
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world'
+  }
+};
+// настройка appolo server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.get('/', (req, res) => res.send('Hello word11'));
+
+app.listen(port, () => {
+  console.log(`GraphQl Server running at http ${port},${server.graphqlPath}`);
+});
